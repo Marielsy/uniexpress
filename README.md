b461a0dca689b23f0ee2029c56358c5579ae2d1a
Marielsy Carvajal <carvajalmarielsy@gmail.com>
2024-02-25 15:53:59 -0500

    first commit, new project

---
+++ README.md
@@ -0,0 +1,3 @@
+# uniexpress
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/uniexpress/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uniexpress
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
