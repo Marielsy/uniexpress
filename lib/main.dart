c7d807c38da3336b5eb757e043ea828380d89d5b
Marielsy Carvajal <carvajalmarielsy@gmail.com>
2024-02-25 17:28:22 -0500

    se agrega la pantalla select route y select bus

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uniexpress/bus/select_bus_screen.dart';
 import 'package:uniexpress/login/login_screen.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.white,
-        body: LoginScreen(),
+        body: SelectBusScreen(),
       ),
     );
   }
