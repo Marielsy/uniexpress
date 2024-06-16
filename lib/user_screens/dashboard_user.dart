f5d1f7fdbd515fca0d44be31823e2e4ddc6ec3ab
Marielsy <carvajalmarielsy@gmail.com>
2024-06-15 21:18:07 -0500

    new changes

--- lib/user_screens/dashboard_user.dart
+++ lib/user_screens/dashboard_user.dart
@@ -78,141 +78,94 @@ class _DashboardUserState extends State<DashboardUser> {
     }
   }
 
-  final marke = [
-    const Marker(
-      width: 200.0,
-      height: 200.0,
-      point: LatLng(9.9105200, -67.3538100),
-      child: Icon(Icons.location_on, color: Colors.blue, size: 50.0),
-    ),
-    const Marker(
-      width: 200.0,
-      height: 200.0,
-      point: LatLng(9.9135200, -67.3548100),
-      child: Icon(Icons.location_on, color: Colors.orange, size: 50.0),
-    ),
-    const Marker(
-      width: 200.0,
-      height: 500.0,
-      point: LatLng(9.9155200, -67.3548100),
-      child: Icon(Icons.location_on, color: Colors.red, size: 50.0),
-    ),
-    const Marker(
-      width: 200.0,
-      height: 200.0,
-      point: LatLng(9.9175200, -67.3548100),
-      child: Icon(Icons.location_on, color: Colors.pink, size: 50.0),
-    ),
-    const Marker(
-      width: 200.0,
-      height: 200.0,
-      point: LatLng(9.9185200, -67.3548100),
-      child: Icon(Icons.person_off_outlined, color: Colors.black, size: 50.0),
-    ),
-    const Marker(
-      width: 200.0,
-      height: 200.0,
-      point: LatLng(9.9165200, -67.3548100),
-      child: Icon(Icons.bus_alert_sharp, color: Colors.black, size: 50.0),
-    ),
-  ];
-
- void _showRouteModal() {
-  showModalBottomSheet(
-    context: context,
-    showDragHandle: true,
-    shape: const RoundedRectangleBorder(
-      borderRadius: BorderRadius.vertical(
-        top: Radius.circular(32.0),
+  void _showRouteModal() {
+    showModalBottomSheet(
+      context: context,
+      showDragHandle: true,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(32.0),
+        ),
       ),
-    ),
-    builder: (BuildContext context) {
-      return Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(6.0),
-            topRight: Radius.circular(16.0),
+      builder: (BuildContext context) {
+        return Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(6.0),
+              topRight: Radius.circular(16.0),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 8.0,
+                offset: const Offset(0, -4),
+              ),
+            ],
           ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.1),
-              blurRadius: 8.0,
-              offset: const Offset(0, -4),
-            ),
-          ],
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Lista de Buses',
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.close),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                ],
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Lista de Buses',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.close),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ],
+                ),
               ),
-            ),
-            const Divider(height: 0),
-            Expanded(
-              child: SelectRouteBottomSheet(
-                routeList: _getRouteList(selectedValueTop),
+              const Divider(height: 0),
+              Expanded(
+                child: SelectRouteBottomSheet(
+                  routeList: _getRouteList(selectedValueTop),
+                ),
               ),
-            ),
-          ],
-        ),
-      );
-    },
-  );
-}
-
+            ],
+          ),
+        );
+      },
+    );
+  }
 
   List<String> _getRouteList(String selectedValue) {
     switch (selectedValue) {
       case 'Agronomia':
         return [
           'T01 Terminal - Agronomía\nterminal, av bolivar, circo'
-          '              10:56am',
-
+              '              10:56am',
           'V02 Villa Olimpica - Agronomía\nterminal, av bolivar, circo'
-          '              10:56am',
+              '              10:56am',
         ];
       case 'Medicina':
         return [
           'T42 Terminal - Medicina\nterminal, av bolivar, circo'
-          '                10:56am',
-          
+              '                10:56am',
           'C07 Casona - Medicina\nterminal, av bolivar, circo'
-          '              10:56am',
-
+              '              10:56am',
         ];
       case 'Ingenieria':
         return [
           'T42 Terminal - Ing Informática\nterminal, av bolivar, circo'
-          '               10:56am',
-
+              '               10:56am',
           'A15 Agronomia - Ingenieria\nterminal, av bolivar, circo'
-          '                     10:56am',
-
+              '                     10:56am',
         ];
       case 'Cs Sociales':
         return [
           'T06 Terminal - Centro\nterminal, av bolivar, circo'
-          '                 10:56am',
-
+              '                 10:56am',
           'C21 Centro - Cs Sociales\nterminal, av bolivar, circo'
-          '             10:56am',
-
+              '             10:56am',
         ];
       default:
         return [];
@@ -323,9 +276,9 @@ class _DashboardUserState extends State<DashboardUser> {
                     ),
                   ],
                 ),
-                 const SizedBox(height: 16.0), 
+                const SizedBox(height: 16.0),
                 DropdownButtonFormField<String>(
-                    decoration: InputDecoration(
+                  decoration: InputDecoration(
                     hintText: 'Elegir Parada...',
                     contentPadding: const EdgeInsets.symmetric(
                         horizontal: 10.0, vertical: 10.0),
@@ -341,7 +294,6 @@ class _DashboardUserState extends State<DashboardUser> {
                     fillColor: Colors.white.withOpacity(0.9),
                   ),
                   dropdownColor: Colors.white,
-
                   value: selectedValueBottom,
                   onChanged: (newValue) {
                     if (newValue != null) {
